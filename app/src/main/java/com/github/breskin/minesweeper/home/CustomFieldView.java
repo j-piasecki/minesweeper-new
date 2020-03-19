48c0ff4a8d508c95d52b15edc62e032dc87dbd7f
j-piasecki <jakubpiasecki67@gmail.com>
2020-03-19 15:06:54 +0100

    Centered sliders in custom field size view

--- app/src/main/java/com/github/breskin/minesweeper/home/CustomFieldView.java
+++ app/src/main/java/com/github/breskin/minesweeper/home/CustomFieldView.java
@@ -47,15 +47,17 @@ public class CustomFieldView extends View {
     public void update() {
         super.update();
 
-        startButton.setPosition(new PointF(RenderView.SIZE * 0.075f, RenderView.VIEW_HEIGHT * 0.9f - startButton.getSize().y));
+        startButton.setPosition(new PointF(RenderView.SIZE * 0.075f, RenderView.VIEW_HEIGHT * 0.925f - startButton.getSize().y));
         startButton.update();
 
         minesSlider.setMinValue(Math.round(widthSlider.getValue() * heightSlider.getValue() * 0.12f));
         minesSlider.setMaxValue(Math.round(widthSlider.getValue() * heightSlider.getValue() * 0.25f));
 
-        minesSlider.getPosition().y = startButton.getPosition().y - minesSlider.getHeight() - RenderView.SIZE * 0.225f;
-        heightSlider.getPosition().y = minesSlider.getPosition().y - minesSlider.getHeight() - RenderView.SIZE * 0.075f;
-        widthSlider.getPosition().y = heightSlider.getPosition().y - heightSlider.getHeight() - RenderView.SIZE * 0.075f;
+        float buttonStart = (startButton.getPosition().y - RenderView.SIZE * 0.25f - minesSlider.getHeight() * 3 - RenderView.SIZE * 0.15f) * 0.5f;
+
+        widthSlider.getPosition().y =  RenderView.SIZE * 0.25f + buttonStart;
+        heightSlider.getPosition().y = widthSlider.getPosition().y + widthSlider.getHeight() + RenderView.SIZE * 0.075f;
+        minesSlider.getPosition().y = heightSlider.getPosition().y + heightSlider.getHeight() + RenderView.SIZE * 0.075f;
 
         widthSlider.update();
         heightSlider.update();
@@ -66,7 +68,7 @@ public class CustomFieldView extends View {
     public void render(Canvas canvas) {
         paint.setColor(Color.WHITE);
         paint.setTextSize(RenderView.SIZE * 0.1f);
-        canvas.drawText(DataManager.CUSTOM_VIEW_HEADER, (RenderView.VIEW_WIDTH - paint.measureText(DataManager.CUSTOM_VIEW_HEADER)) * 0.5f, widthSlider.getPosition().y * 0.5f, paint);
+        canvas.drawText(DataManager.CUSTOM_VIEW_HEADER, (RenderView.VIEW_WIDTH - paint.measureText(DataManager.CUSTOM_VIEW_HEADER)) * 0.5f, RenderView.SIZE * 0.25f, paint);
 
         widthSlider.render(canvas);
         heightSlider.render(canvas);
